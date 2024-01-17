d4a7db0b61f7aa1d5abf9020db6942f524868c0c
Bryan Poblete <bryanpobletereyes@gmail.com>
2024-01-17 10:43:50 -0300

    Actualizacion logica JS

--- scripts.js
+++ scripts.js
@@ -1,11 +1,19 @@
-const inputUser = parseInt(prompt('Ingresa la cantidad de numeros que deseas ver de la secuencia de Fibonacci'));
-const fibonacci = [0,1];
-const domResult = document.getElementById('fibonacci');
+// Logica de la aplicacion
+function calcularFibonacci() {
+    const domResult = document.getElementById('fibonacci');
+    const fibonacci = [0,1];
+    const inputUser = parseInt(prompt('Ingresa la cantidad de numeros que deseas ver de la secuencia de Fibonacci'));
 
-for (let i = 1; i < inputUser-1; i++) {
-    let temp = fibonacci[i] + fibonacci[i-1]
-    fibonacci.push(temp)
-    console.log(fibonacci)
+    if (inputUser === parseInt(inputUser)) {
+        for (let i = 1; i < inputUser-1; i++) {
+            let temp = fibonacci[i] + fibonacci[i-1]
+            fibonacci.push(temp)
+        }
+        domResult.innerText = fibonacci
+    } 
+    else {
+        alert('Invalid Input')
+        calcularFibonacci()
+    }
 }
-
-domResult.innerText = fibonacci
+calcularFibonacci()
